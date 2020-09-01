286695e799166cd4462375d77da0c4510b83398b
Clarence <diyobe2207@gmail.com>
2020-09-01 20:57:51 +0200

    Prototype finished
    - Implemented PlayerController script
    - Implemented NPCBehaviour script
    - Added choices panel for interactions

---
+++ Godfater/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    CircleCollider2D circleCollider;
+
+    float horizontal;
+    float vertical;
+    public float speed = 20.0f;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        circleCollider = GetComponent<CircleCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal * speed * Time.deltaTime, vertical * speed * Time.deltaTime);
+    }
+
+    void Move()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+    }
+}
